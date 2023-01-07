19e8408608014c3967ebac45951e63286e65eeba
bruno <bruno.lorenzo.arroyo@gmail.com>
2023-01-07 20:42:59 +0000

    ver 0.2

--- ull_music.sql
+++ ull_music.sql
@@ -43,6 +43,8 @@ CREATE TABLE servicio_tecnico (
   nombre VARCHAR(50) NOT NULL,
   apellido VARCHAR(50) NOT NULL,
   dni VARCHAR(20) NOT NULL,
+  fecha_de_alta DATE DEFAULT CURRENT_TIMESTAMP,
+  fecha_de_baja DATE NULL,
   PRIMARY KEY (id_tecnico)
 );
 
@@ -75,11 +77,10 @@ CREATE TABLE canciones (
 
 CREATE TABLE listas_de_canciones (
   id_lista INT NOT NULL,
-  id_usuario INT NOT NULL,
   nombre_lista VARCHAR(50) NOT NULL,
+  descripcion VARCHAR(1000) NULL,
   duracion FLOAT NOT NULL,
-  PRIMARY KEY (id_lista, id_usuario),
-  FOREIGN KEY (id_usuario) REFERENCES usuarios (id_usuario)
+  PRIMARY KEY (id_lista)
 );
 
 CREATE TABLE autores (
@@ -105,15 +106,27 @@ CREATE TABLE generos (
   PRIMARY KEY (id_genero)
 );
 
+CREATE TABLE comentarios (
+  id_comentario SERIAL NOT NULL,
+  id_usuario INT NOT NULL,
+  id_cancion INT NOT NULL,
+  texto_comentario VARCHAR(1000) NOT NULL,
+  fecha_comentario DATE DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (id_comentario, id_usuario, id_cancion),
+  FOREIGN KEY (id_usuario) REFERENCES usuarios (id_usuario),
+  FOREIGN KEY (id_cancion) REFERENCES canciones (id_cancion)
+);
+
 /* -- Tabla de relaciones -- */
 
 CREATE TABLE resuelve (
   id_tecnico INT NOT NULL,
   id_problema INT NOT NULL,
   id_usuario INT NOT NULL,
-  fecha_resolucion DATE DEFAULT CURRENT_TIMESTAMP,
+  fecha_incidencia DATE NOT NULL,
+  fecha_resolucion DATE NOT NULL,
   resuelto BOOLEAN NOT NULL,
-  PRIMARY KEY (id_tecnico, id_problema, id_usuario, fecha_resolucion),
+  PRIMARY KEY (id_tecnico, id_problema, id_usuario),
   FOREIGN KEY (id_tecnico) REFERENCES servicio_tecnico (id_tecnico),
   FOREIGN KEY (id_problema) REFERENCES problema (id_problema),
   FOREIGN KEY (id_usuario) REFERENCES usuarios (id_usuario)
@@ -124,7 +137,15 @@ CREATE TABLE lista_canciones_canciones (
   id_cancion INT NOT NULL,
   PRIMARY KEY (id_lista, id_cancion),
   FOREIGN KEY (id_lista) REFERENCES listas_de_canciones (id_lista),
-  FOREIGN KEY (id_cancion) REFERENCES canciones (id_cancion),
+  FOREIGN KEY (id_cancion) REFERENCES canciones (id_cancion)
+);
+
+CREATE TABLE lista_canciones_usuarios (
+  id_lista INT NOT NULL,
+  id_usuario INT NOT NULL,
+  PRIMARY KEY (id_lista, id_usuario),
+  FOREIGN KEY (id_lista) REFERENCES listas_de_canciones (id_lista),
+  FOREIGN KEY (id_usuario) REFERENCES usuarios (id_usuario)
 );
 
 CREATE TABLE usuario_canciones (
@@ -190,3 +211,341 @@ CREATE TABLE artista (
 );
 
 /* -- INSERT DATA -- */
+
+INSERT INTO servicio_tecnico (id_tecnico, nombre, apellido, dni, fecha_de_alta, fecha_de_baja) VALUES
+  (1, 'Roberto', 'Mascarpone Olivares', '12345678P', '02-01-2022', NULL),
+  (2, 'Juan', 'Perez Reverte', '12345679A', '02-01-2022', NULL),
+  (3, 'Andrés', 'González González', '12345678B', '02-01-2022', NULL);
+
+INSERT INTO problema (id_problema, nombre_problema, descripcion, resolucion) VALUES
+  (1, 'No se puede iniciar sesión', 'No se puede iniciar sesión en la aplicación', 'Reiniciar la aplicación'),
+  (2, 'No se puede reproducir una canción', 'No se puede reproducir una canción', 'Reiniciar la aplicación'),
+  (3, 'No se puede reproducir un álbum', 'No se puede reproducir un álbum', 'Reiniciar la aplicación'),
+  (4, 'No se puede reproducir una lista de reproducción', 'No se puede reproducir una lista de reproducción', 'Reiniciar la aplicación');
+
+
+INSERT INTO usuarios (id_usuario, nombre, apellido, dni, username, email, fecha_registro) VALUES
+  (1, 'Bruno Lorenzo', 'Arroyo Pedraza', '12345678C', 'brunolarroyo', 'bruno@gmail.com', '02-01-2022'),
+  (2, 'Carla Cristina', 'Olivares Rodríguez', '12345679D', 'carlacolivares', 'carla@gmail.com', '02-01-2022'),
+  (3, 'Dana Belén', 'Choque Zárate', '12345678E', 'danabelenchoque', 'dana@gmail.com', '02-01-2022');
+
+
+INSERT INTO canciones (id_cancion, nombre_cancion, año_salida, duracion) VALUES
+  (1, 'Que te vaya mal', 2022, 3.39), -- ALL YOU CAN EAT (COMIDA PARA LLEVAR)
+  (2, 'Los Gatos', 2022, 3.16), -- ALL YOU CAN EAT (COMIDA PARA LLEVAR)
+  (3, 'Gratiné', 2022, 2.23), -- ALL YOU CAN EAT (COMIDA PARA LLEVAR)
+  (4, 'Get Into It', 2021, 2.18), -- PLANET HER (DOJA CAT)
+  (5, 'Woman', 2021, 2.52), -- PLANET HER (DOJA CAT)
+  (6, 'Ojitos lindos', 2022, 4.18), -- UN VERANO SIN TI (BAD BUNNY)
+  (7, 'Neverita', 2022, 2.53), -- UN VERANO SIN TI (BAD BUNNY)
+  (8, 'Me Fui De Vacaciones', 2022, 3.00), -- UN VERANO SIN TI (BAD BUNNY)
+  (9, 'Cuando Olvidaré', 2021, 3.09), -- EL MADRILEÑO (C. TANGANA)
+  (10, 'Los Tontos', 2021, 3.12), -- EL MADRILEÑO (C. TANGANA)
+  (11, 'Bliss', 2001, 4.11), -- ORIGIN OF SYMMETRY (MUSE)
+  (12, 'New Born', 2001, 6.03), -- ORIGIN OF SYMMETRY (MUSE)
+  (13, 'Megalomania', 2001, 4.39), -- ORIGIN OF SYMMETRY (MUSE)
+  (14, 'Lone Digger', 2015, 3.49), -- <|º_º|> (CARAVAN PALACE)
+  (15, 'Wonderland', 2015, 3.10), -- <|º_º|> (CARAVAN PALACE)
+  (16, 'Egotistic', 2018, 3.16), -- RED MOON (MAMAMOO)
+  (17, 'Sleep In The Car', 2018, 3.22), -- RED MOON (MAMAMOO)
+  (18, 'Emperors New Clothes', 2016, 2.38), -- DEATH OF A BACHELOR (PANIC! AT THE DISCO)
+  (19, 'Golden Days', 2016, 4.14), -- DEATH OF A BACHELOR (PANIC! AT THE DISCO)
+  (20, 'Victorious', 2016, 2.58); -- DEATH OF A BACHELOR (PANIC! AT THE DISCO)
+  
+INSERT INTO listas_de_canciones (id_lista,nombre_lista,descripcion, duracion) VALUES
+  (1, 'Clásicos del rock', 'Una lista de reproducción con algunos de los grandes éxitos del rock de todos los tiempos', 0),
+  (2, 'Hits del pop', 'Una lista de reproducción con algunas de las canciones más populares del pop actual', 0),
+  (3, 'Lo mejor del hip hop', 'Una lista de reproducción con algunos de los mejores éxitos del hip hop de todos los tiempos', 0),
+  (4, 'Éxitos del K-Pop', 'Una lista de reproducción con algunas de las mejores canciones de K-pop', 0),
+  (5, 'Electrónica para bailar', 'Una lista de reproducción con algunos de los éxitos más populares de la música electrónica para bailar', 0),
+  (6, 'Lo más nuevo del reggeatón', 'Una lista de reproducción con las canciones más recientes y populares del reggeatón', 0),
+  (7, 'Indie rock', 'Una lista de reproducción con algunos de los grandes éxitos del indie rock', 0);
+
+INSERT INTO autores (id_autor, nombre_autor, discografia) VALUES 
+  (1, 'Motherflowers', NULL),
+  (2, 'Doja Cat', 'RCA Records'),
+  (3, 'Bad Bunny', 'Rimas Entertainment'),
+  (4, 'C. Tangana', 'Sony Music'),
+  (5, 'MUSE', 'Warner Records'),
+  (6, 'Caravan Palace', 'Le Plan Recordings'),
+  (7, 'MAMAMOO', 'RBW Entertainment'),
+  (8, 'PANIC! AT THE DISCO', 'Atlantic Records'); 
+
+
+INSERT INTO albumes (id_album, id_autor, nombre_album, año_salida, duracion) VALUES
+  (1, 1, 'ALL YOU CAN EAT', 2022, 0),
+  (2, 2, 'PLANET HER', 2021, 0),
+  (3, 3, 'UN VERANO SIN TI', 2022, 0),
+  (4, 4, 'EL MADRILEÑO', 2021, 0),
+  (5, 5, 'ORIGIN OF SYMMETRY', 2001, 0),
+  (6, 6, '<|º_º|>', 2022, 0),
+  (7, 7, 'RED MOON', 2022, 0),
+  (8, 8, 'DEATH OF A BACHELOR', 2016, 0);
+
+INSERT INTO generos (id_genero, nombre_genero) VALUES 
+  (1, 'Rock'),
+  (2, 'Pop'),
+  (3, 'Jazz'),
+  (4, 'Metal'),
+  (5, 'Clásica'),
+  (6, 'Reggae'),
+  (7, 'Salsa'),
+  (8, 'Folk'),
+  (9, 'Blues'),
+  (10, 'Rap'),
+  (11, 'Electrónica'),
+  (12, 'Funk'),
+  (13, 'Soul'),
+  (14, 'Country'),
+  (15, 'Reggaeton'),
+  (16, 'Disco'),
+  (17, 'Indie'),
+  (18, 'Punk'),
+  (19, 'Hip Hop'),
+  (20, 'Alternativa'),
+  (21, 'Heavy Metal'),
+  (22, 'Cumbia'),
+  (23, 'Flamenco'),
+  (24, 'Bachata'),
+  (25, 'Merengue'),
+  (26, 'Ska'),
+  (27, 'Música Clásica'),
+  (28, 'Música Urbana'),
+  (29, 'Música Tropical'),
+  (30, 'Música de Cine'),
+  (31, 'Música de Baile'),
+  (32, 'Kpop');
+
+
+INSERT INTO comentarios (id_comentario, id_usuario, id_cancion, texto_comentario, fecha_comentario) VALUES 
+  (1, 1, 1, 'Esta canción tiene un ritmo increíble, me encanta bailarla', '22-07-2022'),
+  (2, 1, 1, 'Me encanta la letra de esta canción, me siento muy identificado con ella', '14-05-2022'),
+  (3, 1, 1, 'La voz del cantante en esta canción es impresionante, me emociona cada vez que la escucho', '17-09-2022'),
+  (4, 1, 1, 'Este es uno de mis artistas favoritos y esta canción es uno de mis temas preferidos de ellos', '05-12-2022'),
+  (5, 1, 1, 'Me encanta la mezcla de géneros en esta canción, es algo completamente nuevo para mí', '28-07-2022'),
+  (6, 1, 1, 'La producción de esta canción es impresionante, me encanta cómo suena', '12-03-2022'),
+  (7, 2, 1, 'La letra de esta canción es muy profunda y me hace reflexionar cada vez que la escucho', '09-08-2022'),
+  (8, 2, 1, 'Me encanta el aire nostálgico de esta canción, me transporta a otro lugar y tiempo', '23-11-2022'),
+  (9, 2, 1, 'La combinación de voces en esta canción es increíble, realmente siento la química entre los artistas', '06-10-2022'),
+  (10, 2, 1, 'Esta La melodía de esta canción es tan pegajosa, no puedo dejar de tararearla tiene una energía increíble, me hace sentir muy vivo', '20-06-2022'),
+  (11, 2, 1, 'La melodía de esta canción es tan pegajosa, no puedo dejar de tararearla', '15-04-2022'),
+  (12, 2, 1, 'Me encanta la forma en que esta canción me hace sentir, es tan emotiva', '18-07-2022'),
+  (13, 2, 1, 'Este es un artista tan talentoso, cada canción que saca es una joya', '06-01-2022'),
+  (14, 3, 1, 'La letra de esta canción es tan honesta y auténtica, realmente siento que me está hablando a mí', '30-09-2022'),
+  (15, 3, 1, 'La música de esta canción es tan original, es algo que nunca he escuchado antes', '02-11-2022'),
+  (16, 3, 1, 'Me encanta la forma en que esta canción me hace moverme, es tan pegajosa', '25-12-2022'),
+  (17, 3, 1, 'La voz del cantante en esta canción es tan potente y emotiva, me siento conectado a ella', '16-02-2022'),
+  (18, 3, 1, 'La letra de esta canción es tan profunda y significativa, me hace pensar en mi vida y en las decisiones que he tomado', '08-03-2022'),
+  (19, 3, 1, 'Esta canción de K-pop tiene un ritmo increíble, me encanta cantarla', '22-07-2022'),
+  (20, 3, 1, 'La música de esta canción es tan pegajosa, no puedo dejar de escucharla una y otra vez', '21-06-2022'),
+  (21, 3, 1, 'La letra de esta canción es tan emotiva, me hace sentir conectado a ella', '29-09-2022'),
+  (22, 3, 1, 'Muy buena canción', '09-03-2022');
+
+INSERT INTO resuelve (id_tecnico, id_problema, id_usuario, fecha_incidencia, fecha_resolucion, resuelto) VALUES
+  (1, 1, 1, '09-03-2022', '09-03-2022', true),
+  (1, 2, 3, '09-04-2022', '10-04-2022', true),
+  (1, 3, 2, '09-05-2022', '11-05-2022', true),
+  (2, 2, 3, '20-03-2022', '21-03-2022', true),
+  (2, 1, 3, '24-03-2022', '24-03-2022', true),
+  (2, 4, 3, '1-03-2022',  '01-03-2022', true),
+  (3, 2, 3, '09-07-2022', '09-07-2022', true),
+  (3, 1, 3, '10-04-2022', '10-04-2022', true),
+  (3, 3, 3, '11-12-2022', '11-12-2022', true);
+
+INSERT INTO lista_canciones_canciones (id_lista, id_cancion) VALUES
+  (1, 11),
+  (1, 12),
+  (1, 13),
+  (1, 18),
+  (1, 19),
+  (1, 20),
+  (2, 4),
+  (2, 5),
+  (3, 1),
+  (3, 2),
+  (3, 3),
+  (3, 9),
+  (3, 10),
+  (4, 16),
+  (4, 17),
+  (5, 14),
+  (5, 15),
+  (6, 6),
+  (6, 7),
+  (6, 8);
+
+INSERT INTO lista_canciones_usuarios (id_lista, id_usuario) VALUES
+  (1, 1),
+  (1, 2),
+  (2, 3),
+  (2, 1),
+  (3, 2),
+  (4, 3),
+  (5, 1),
+  (6, 2),
+  (7, 3);
+
+INSERT INTO usuario_canciones (id_usuario, id_cancion) VALUES
+  (1, 5),
+  (1, 8),
+  (1, 11),
+  (1, 13),
+  (1, 14),
+  (1, 15),
+  (1, 18),
+  (1, 2),
+  (2, 1),
+  (2, 2),
+  (2, 3),
+  (2, 4),
+  (2, 6),
+  (2, 7),
+  (2, 8),
+  (2, 12),
+  (2, 20),
+  (3, 19),
+  (3, 16),
+  (3, 17),
+  (3, 8),
+  (3, 9),
+  (3, 1),
+  (3, 10),
+  (3, 5),
+  (3, 4);
+
+INSERT INTO canciones_albumes (id_cancion, id_album) VALUES 
+  (1, 1),
+  (2, 1),
+  (3, 1),
+  (4, 2),
+  (5, 2),
+  (6, 3),
+  (7, 3),
+  (8, 3),
+  (9, 4),
+  (10, 4),
+  (11, 5),
+  (12, 5),
+  (13, 5),
+  (14, 6),
+  (15, 6),
+  (16, 7),
+  (17, 7),
+  (18, 8),
+  (19, 8),
+  (20, 8);
+
+INSERT INTO canciones_autores (id_cancion, id_autor) VALUES
+  (1, 1),
+  (2, 1),
+  (3, 1),
+  (4, 2),
+  (5, 2),
+  (6, 3),
+  (7, 3),
+  (8, 3),
+  (9, 4),
+  (10, 4),
+  (11, 5),
+  (12, 5),
+  (13, 5),
+  (14, 6),
+  (15, 6),
+  (16, 7),
+  (17, 7),
+  (18, 8),
+  (19, 8),
+  (20, 8);
+
+INSERT INTO canciones_generos (id_cancion, id_genero) VALUES
+  (1, 10),
+  (1, 19),
+  (2, 19),
+  (3, 19),
+  (4, 2),
+  (4, 19),
+  (4, 20),
+  (5, 2),
+  (6, 15),
+  (6, 11),
+  (7, 15),
+  (7, 11),
+  (8, 15),
+  (8, 6),
+  (9, 19),
+  (10, 19),
+  (11, 1),
+  (12, 1),
+  (13, 1),
+  (14, 11),
+  (15, 11),
+  (16, 7),
+  (17, 32),
+  (18, 32),
+  (19, 1),
+  (19, 2),
+  (20, 1),
+  (20, 2);
+
+INSERT INTO autores_generos (id_autor, id_genero) VALUES
+  (1, 10),
+  (1, 19),
+  (2, 2),
+  (2, 19),
+  (2, 20),
+  (2, 16),
+  (3, 15),
+  (3, 11),
+  (3, 10),
+  (4, 19),
+  (5, 1),
+  (5, 11),
+  (6, 11),
+  (7, 32),
+  (8, 1),
+  (8, 2);
+
+INSERT INTO albumes_generos (id_album, id_genero) VALUES
+  (1, 10),
+  (1, 19),
+  (2, 2),
+  (2, 19),
+  (2, 20),
+  (3, 15),
+  (3, 11),
+  (3, 6),
+  (4, 19),
+  (5, 1),
+  (6, 11),
+  (7, 7),
+  (7, 32),
+  (8, 1),
+  (8, 2),
+  (8, 32);
+
+INSERT INTO grupo (id_autor, nombre_integrante) VALUES
+  (1, 'Irepelusa'),
+  (1, 'Veztalone'),
+  (1, 'Frank Lucas'),
+  (1, 'Tayko'),
+  (5, 'Matt Bellamy'),
+  (5, 'Chris Wolstenholme'),
+  (5, 'Dominic Howard'),
+  (6, 'Zoé Colotis'),
+  (6, 'Arnaud Vial'),
+  (6, 'Hugues Payen'),
+  (6, 'Camille Chapelière'),
+  (6, 'Charles Delaporte'),
+  (6, 'Antoine Toustou'),
+  (6, 'Paul-Marie Barbier'),
+  (6, 'Victor Raimondeau'),
+  (7, 'Moonbyul'),
+  (7, 'Solar'),
+  (7, 'Wheein'),
+  (7, 'Hwasa');
+
+INSERT INTO artista (id_autor, nombre_artista) VALUES
+  (2, 'Doja Cat'),
+  (3, 'Bad Bunny'),
+  (4, 'C. Tangana'),
+  (8, 'Brendon Urie');
